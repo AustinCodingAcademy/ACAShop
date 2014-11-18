127fbfb4e13b04fb9db323a5d4a711395a166bb2
Samir Patel <sameg14@gmail.com>
2014-11-18 14:14:37 -0600

    Mo werk

--- src/ACA/ShopBundle/Controller/CartController.php
+++ src/ACA/ShopBundle/Controller/CartController.php
@@ -2,7 +2,11 @@
 
 namespace ACA\ShopBundle\Controller;
 
+use ACA\ShopBundle\Shop\Product;
+use ACA\ShopBundle\Shop\DBCommon;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class CartController responsible for displaying and handling all shopping cart related functionality
@@ -17,7 +21,41 @@ class CartController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('ACAShopBundle:Cart:index.html.twig');
+        $Session = new Session();
+        $Session->start();
+
+        /** @var DBCommon $db */
+        $db = $this->get('db');
+
+        /**
+         * Array of all productIds added to the shopping cart
+         *
+         * @var array
+         */
+        $cartItems = $Session->get('cart_items');
+
+        // Make sure that we have no duplicates
+        $cartItems = !empty($cartItems) ? array_unique($cartItems) : null;
+
+        /** @var Product[] $Products Array of all Product objects added to the user's cart */
+        $Products = array();
+
+        if(!empty($cartItems)){
+
+            foreach ($cartItems as $productId) {
+                $Product = new Product($productId);
+                $Product->setDb($db);
+                $Product->load();
+                $Products[] = $Product;
+            }
+        }
+
+        return $this->render(
+            'ACAShopBundle:Cart:index.html.twig',
+            array(
+                'Products' => $Products
+            )
+        );
     }
 
     /**
@@ -25,7 +63,45 @@ class CartController extends Controller
      */
     public function addAction()
     {
-        $productId = $_POST['product_id'];
-        pre($productId, '$productId');
+        $Request = new Request();
+        $productId = $Request->get('product_id');
+
+        $Session = new Session();
+        $Session->start();
+
+        $cartItems = $Session->get('cart_items');
+
+        if (empty($cartItems)) {
+            $cartItems = array($productId);
+        } else {
+            $cartItems[] = $productId;
+        }
+
+        $Session->set('cart_items', $cartItems);
+
+        return $this->redirect('/cart');
+    }
+
+    /**
+     * Remove a product from the shopping cart
+     */
+    public function removeAction()
+    {
+        $Request = new Request();
+        $productId = $Request->get('product_id');
+
+        $Session = new Session();
+        $Session->start();
+
+        $cartItems = $Session->get('cart_items');
+
+        foreach($cartItems as $k => $v){
+            if($v == $productId){
+                unset($cartItems[$k]);
+            }
+        }
+        $Session->set('cart_items', $cartItems);
+
+        return $this->redirect('/cart');
     }
 }
